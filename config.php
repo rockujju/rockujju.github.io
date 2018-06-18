5f5ba76052fa97c41e4703407779eedeefa130af
Ujjawal Kumar <31826257+rockujju@users.noreply.github.com>
2018-06-18 16:45:19 +0530

    initial commit

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$connection = mysqli_connect('rds-mysql-rockujju.ckfl83e5z6wb.us-west-1.rds.amazonaws.com:3306', 'mysql_rockujju', 'mysql_ujju7630505!');
-if (!$connection){
-    die("Database Connection Failed" . mysqli_error($connection));
-}
-$select_db = mysqli_select_db($connection, 'Test');
-if (!$select_db){
-    die("Database Selection Failed" . mysqli_error($connection));
-}
